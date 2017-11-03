58ecb2a1ff45176c098452a542ba77f724902297
piggyvenus <piggyvenus@gmail.com>
2017-11-02 21:19:03 -0400

    data init container test

--- data/init.sh
+++ data/init.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-while true; do echo -e "HTTP/1.1 200 OK\n\n$(date)\nApplication Version 1: Host: $(tail -1 /etc/hosts| awk '{print $2}')" | nc -ll -p 8080; done
+cp index.html /opt/data/
+#while true; do echo -e "HTTP/1.1 200 OK\n\n$(date)\nApplication Version 1: Host: $(tail -1 /etc/hosts| awk '{print $2}')" | nc -ll -p 8080; done
 
