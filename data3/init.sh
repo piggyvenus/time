8a143c4dd19a6555e193077f1d7aa17564e0f4a1
piggyvenus <piggyvenus@gmail.com>
2017-11-03 01:06:18 -0400

    data init container test

--- data3/init.sh
+++ data3/init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cp /opt/data/index2.html /usr/share/nginx/html/index.html
+cp index.html /usr/share/nginx/html/index.html
 while true; do echo -e "HTTP/1.1 200 OK\n\n$(date)\nApplication Version 1: Host: $(tail -1 /etc/hosts| awk '{print $2}')" | nc -ll -p 8080; done
 
